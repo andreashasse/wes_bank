66fd464359b344c0ed9f951e30cdd1b47ad5c3c7
Andreas Hasselberg <andreas.hasselberg@gmail.com>
2014-01-30 10:48:56 +0100

    Changed the wes_bank api.

--- src/wes_bank.erl
+++ src/wes_bank.erl
@@ -1,32 +1,51 @@
 -module(wes_bank).
 
--export([start_wes/1,
+-export([start_session/1,
+         open_account/2,
          transfer/4,
          insert/3,
          withdraw/3,
-         balance/1]).
+         balance/1,
+         test/0]).
 
-start_wes(Session) ->
-    wes_channel:start(bank_session, Session, []).
+start_session(Session) ->
+    {ok, _} = wes_channel:start(bank_session, Session, []).
+
+stop_session(Session) ->
+    wes_channel:stop(bank_session, Session).
 
 insert(Session, To, Amount) when Amount > 0 ->
     Payload = {To, Amount},
-    start_wes(Session),
-    ok = wes_channel:register_actor(bank_session, Session, account, To, [To]),
     ok = wes_channel:command(bank_session, Session, insert, Payload).
 
 withdraw(Session, To, Amount) when Amount >  0 ->
     Payload = {To, Amount},
-    start_wes(Session),
-    ok = wes_channel:register_actor(bank_session, Session, account, To, [To]),
     ok = wes_channel:command(bank_session, Session, withdraw, Payload).
 
 transfer(Session, From, To, Amount) when Amount > 0 ->
     Payload = {From, To, Amount},
-    start_wes(Session),
-    ok = wes_channel:register_actor(bank_session, Session, account, From, [From]),
-    ok = wes_channel:register_actor(bank_session, Session, account, To, [To]),
     ok = wes_channel:command(bank_session, Session, transfer, Payload).
 
 balance(Name) ->
     wes_channel:read(bank_session, account, Name, balance).
+
+open_account(Session, Account) ->
+    ok = wes_channel:register_actor(bank_session, Session, account,
+                                    Account, [Account]).
+
+test() ->
+    Session = andreas,
+    start_session(Session),
+    open_account(Session, a1),
+    open_account(Session, a2),
+    error_logger:info_msg("Balance 1 a1 ~p", [balance(a1)]),
+    error_logger:info_msg("Balance 1 a2 ~p", [balance(a2)]),
+    insert(Session, a1, 10),
+    insert(Session, a2, 5),
+    error_logger:info_msg("Balance 2 a1 ~p", [balance(a1)]),
+    error_logger:info_msg("Balance 2 a2 ~p", [balance(a2)]),
+    transfer(Session, a1, a2, 1),
+    error_logger:info_msg("Balance 3 a1 ~p", [balance(a1)]),
+    error_logger:info_msg("Balance 3 a2 ~p", [balance(a2)]),
+    stop_session(Session),
+    ok.
